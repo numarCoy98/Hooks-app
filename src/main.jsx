e65390cfb0c99a9ef5c15881233bbf79614d677e
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-04-20 09:48:22 -0500

    instalar react-router-dom y configurarlo

--- src/main.jsx
+++ src/main.jsx
@@ -14,11 +14,15 @@ import './index.css'
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { Padre } from './07-tarea-memo/Padre';
 // import './08-useReducer/intro-reducer'
+// import { TodoApp } from './08-useReducer/TodoApp';
 
-import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
+
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <TodoApp />
-    </>
+    <BrowserRouter>
+
+        <MainApp />
+    </BrowserRouter>
 )
